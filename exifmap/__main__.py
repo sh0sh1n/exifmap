fdb1dab5dfa9b1a71e3eb24d480ce8f163eab362
Shohan Hasan <shohan.hasan.x6@gmail.com>
2019-10-02 16:30:58 -0400

    Add API calls to plot map

--- exifmap/__main__.py
+++ exifmap/__main__.py
@@ -1,4 +1,51 @@
+import sys
+import os
+import webbrowser
 import exifmap.exifmap as em
+import creds
+
+
+def help():
+    print("Usage: exifmap <command> [<args>]\n")
+    print("Commands:")
+    print("\tdata\t Outputs space delimited table of data parsed from each file.")
+    print(
+        "\tmap\t Opens a web browser to display a static image of locations scraped from file."
+    )
+    # print("\turl\t Outputs the query URL to generate static map.")
+
 
 if __name__ == "__main__":
-    em.map(".")
+    if len(sys.argv) < 2:
+        help()
+        sys.exit()
+
+    command = str(sys.argv[1])
+
+    opt = str(sys.argv[2]) if len(sys.argv) > 2 else None
+    if command == "data":
+        folder = "." if opt is None else opt
+        e = em.exif(folder)
+        for fn, data in e.items():
+            lat = data.get("latitude")
+            lon = data.get("longitude")
+            print(f"{fn} {lat} {lon}")
+
+    elif command == "map":
+        api_key = (
+            os.environ["GMAPS_API_KEY"]
+            if "GMAPS_API_KEY" in os.environ
+            else creds.get_api_key()
+        )
+
+        if api_key is None:
+            raise Exception(
+                "Please set 'GMAPS_API_KEY' environment variable or define"
+                "the api_key() function in creds.py to return the API KEY."
+            )
+
+        url = em.map_url(em.exif(opt), api_key)
+        # print(url)
+        webbrowser.open(url)
+    else:
+        help()
--- exifmap/exifmap.py
+++ exifmap/exifmap.py
@@ -1,16 +1,83 @@
 from exif import Image
 import os
+import re
+from urllib.parse import urlencode
+
+# Get a query URL for Google Maps Static API
+def map_url(data, api_key, width=500, height=500, map_type="roadmap"):
+    base_url = "https://maps.googleapis.com/maps/api/staticmap?"
+    params = {
+        "size": f"{width}x{height}",
+        "key": api_key,
+        "maptype": map_type,
+        "markers": [],
+    }
+    for f, d in data.items():
+        lat = d.get("latitude")
+        lon = d.get("longitude")
+        if lat is None or lon is None:
+            continue
+
+        params["markers"].append("{0:.6f},{1:.6f}".format(lat, lon))
+
+    params["markers"] = "|".join(params["markers"])
+    param_str = urlencode(params)
+    full_url = base_url + param_str
+    print(full_url)
+    return full_url
 
 
-def map(folder):
+# Construct base marker query param
+# TODO: implement
+def _markerq_base(size="normal", color=None, label=None):
+    pass
+
+
+# Return a dictionary with geo-locations of files rooted at given directory
+def exif(folder, recurse=True):
+    data = dict()
     for root, dirs, files in os.walk(folder):
         for fname in files:
-            print(os.path.join(root, fname))
-            with open(os.path.join(root, fname), "rb") as f:
+            fpath = os.path.join(root, fname)
+            with open(fpath, "rb") as f:
                 img = Image(f)
                 if not img.has_exif:
                     continue
 
-                print("({img.gps_latitude}, {img.gps_longitude})")
-        for dname in dirs:
-            print(os.path.join(root, dname))
+                data[fpath] = _get_geo(img)
+    return data
+
+
+# Given an image return the dictionary containing relevant exif data
+def _get_geo(img):
+    d = {
+        "latitude": _to_decimal(_get_exif_attr(img, "gps_latitude")),
+        "longitude": _to_decimal(_get_exif_attr(img, "gps_longitude")),
+        "time": _get_exif_time(img),
+    }
+    return d
+
+
+# Convert degrees, minutes, seconds to decimal
+def _to_decimal(geo):
+    if geo is None:
+        return None
+
+    s = re.sub("[^0-9,.]", "", str(geo))
+    bits = [float(x) for x in s.split(",")]
+    d = sum([b / s for b, s in zip(bits, [1, 60.0, 3600.0])])
+    return d
+
+
+# Get an attribute from exif if available, None otherwise
+def _get_exif_attr(img, attr):
+    if attr in dir(img):
+        return getattr(img, attr)
+    else:
+        return None
+
+
+# Scan through different exif metadata fields for the
+# earliest creation time
+def _get_exif_time(img):
+    pass
